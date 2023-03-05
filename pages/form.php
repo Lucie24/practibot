d3bb1cc70e52f1ae8aa7dc95de1ae7138ec77919
Lucie <luciedumas24430@gmail.com>
2023-03-05 12:37:01 +0100

    insertion liste départementale et symptômes

--- pages/form.php
+++ pages/form.php
@@ -12,7 +12,7 @@
 ?>
 
 <!-- ======= Appointment Section ======= -->
-    <main id="appointment" class="marginFatTop marginThinBottom appointment backgroundCover">
+    <main id="appointment" class="marginFatTop marginThinBottom appointment">
       <div class="container">
 
         <div class="section-title">
@@ -44,12 +44,11 @@
               <div class="validate"></div>
             </div>
             <div class="col-md-4 form-group mt-3">
-              <select name="department" id="department" class="form-select">
-                <option value="">Sélectionner votre département</option>
-                <option value="Department 1">Department 1</option>
-                <option value="Department 2">Department 2</option>
-                <option value="Department 3">Department 3</option>
-              </select>
+
+              <?php
+              include "../components/liste_dep.html";
+              ?>
+
               <div class="validate"></div>
             </div>
             <div class="col-md-4 form-group mt-3">
@@ -65,57 +64,59 @@
 
             <p class="marginThinTop">Symptômes :</p>
 
-                     <div class="flexRow spaceBetween">
+            <div class="flexRow spaceBetween">
 
-            <div>
-                <input type="checkbox" id="s1" name="s1">
-                <label for="scales">Symptôme 1</label>
-            </div>
+              <div>
+                  <input type="checkbox" id="s1" name="s1">
+                  <label for="scales">Maux de tête</label>
+              </div>
 
-            <div>
-                <input type="checkbox" id="s4" name="s4">
-                <label for="horns">Symptôme 4</label>
-            </div>
+              <div>
+                  <input type="checkbox" id="s4" name="s4">
+                  <label for="horns">Saignements</label>
+              </div>
 
-            <div>
-                <input type="checkbox" id="s7" name="s7">
-                <label for="horns">Symptôme 7</label>
-            </div>
-                        <div>
-                <input type="checkbox" id="s10" name="s10">
-                <label for="horns">Symptôme 10</label>
-            </div>
-                        <div>
-                <input type="checkbox" id="s13" name="s13">
-                <label for="horns">Symptôme 13</label>
-            </div>
+              <div>
+                  <input type="checkbox" id="s7" name="s7">
+                  <label for="horns">Toux</label>
+              </div>
+
+              <div>
+                  <input type="checkbox" id="s10" name="s10">
+                  <label for="horns">Fièvre</label>
+              </div>
+              
+              <div>
+                  <input type="checkbox" id="s13" name="s13">
+                  <label for="horns">Malaise</label>
+              </div>
 
             </div>
 
             <div class="flexRow spaceBetween">
 
-            <div>
-                <input type="checkbox" id="s2" name="s2">
-                <label for="scales">Symptôme 2</label>
-            </div>
+              <div>
+                  <input type="checkbox" id="s2" name="s2">
+                  <label for="scales">Ecoulement nasal</label>
+              </div>
 
-            <div>
-                <input type="checkbox" id="s5" name="s5">
-                <label for="horns">Symptôme 5</label>
-            </div>
+              <div>
+                  <input type="checkbox" id="s5" name="s5">
+                  <label for="horns">Tachicardie</label>
+              </div>
 
-            <div>
-                <input type="checkbox" id="s8" name="s8">
-                <label for="horns">Symptôme 8</label>
-            </div>
-                        <div>
-                <input type="checkbox" id="s11" name="s11">
-                <label for="horns">Symptôme 11</label>
-            </div>
-                        <div>
-                <input type="checkbox" id="s14" name="s14">
-                <label for="horns">Symptôme 14</label>
-            </div>
+              <div>
+                  <input type="checkbox" id="s8" name="s8">
+                  <label for="horns">Douleur à la poitrine</label>
+              </div>
+                          <div>
+                  <input type="checkbox" id="s11" name="s11">
+                  <label for="horns">Difficulté respiratoire</label>
+              </div>
+                          <div>
+                  <input type="checkbox" id="s14" name="s14">
+                  <label for="horns">Symptôme 14</label>
+              </div>
 
             </div>
 
