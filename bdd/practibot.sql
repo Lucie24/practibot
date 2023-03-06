194d5f55729a48f2674099c45a9f2375cf9db3a9
Lucie <luciedumas24430@gmail.com>
2023-03-06 19:38:43 +0100

    création de la BDD

--- bdd/practibot.sql
+++ bdd/practibot.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 28 Février 2023 à 09:41
+-- Généré le :  Lun 06 Mars 2023 à 18:37
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -32,7 +32,43 @@ CREATE TABLE `patient` (
   `id_patient` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
-  `age` int(11) NOT NULL
+  `age` int(3) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `tel` int(10) DEFAULT NULL,
+  `departement` int(3) NOT NULL,
+  `medecin_traitant` int(4) NOT NULL,
+  `s1` tinyint(1) NOT NULL,
+  `s2` tinyint(1) NOT NULL,
+  `s3` tinyint(1) NOT NULL,
+  `s4` tinyint(1) NOT NULL,
+  `s5` tinyint(1) NOT NULL,
+  `s6` tinyint(1) NOT NULL,
+  `s7` tinyint(1) NOT NULL,
+  `s8` tinyint(1) NOT NULL,
+  `s9` tinyint(1) NOT NULL,
+  `s10` tinyint(1) NOT NULL,
+  `s11` tinyint(1) NOT NULL,
+  `s12` tinyint(1) NOT NULL,
+  `s13` tinyint(1) NOT NULL,
+  `s14` tinyint(1) NOT NULL,
+  `s15` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `praticien`
+--
+
+CREATE TABLE `praticien` (
+  `id_praticien` int(11) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `specialite` int(1) NOT NULL,
+  `departement` int(3) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `mdp` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -46,6 +82,12 @@ ALTER TABLE `patient`
   ADD PRIMARY KEY (`id_patient`);
 
 --
+-- Index pour la table `praticien`
+--
+ALTER TABLE `praticien`
+  ADD PRIMARY KEY (`id_praticien`);
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
@@ -54,6 +96,11 @@ ALTER TABLE `patient`
 --
 ALTER TABLE `patient`
   MODIFY `id_patient` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `praticien`
+--
+ALTER TABLE `praticien`
+  MODIFY `id_praticien` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
