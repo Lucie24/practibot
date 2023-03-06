de5148e8fa3564efba407f4673c2280848908ee0
Lucie <luciedumas24430@gmail.com>
2023-03-06 22:31:01 +0100

    Lien entre le formulaire de connexion et la base de donnée effectué (avec vérifications)

--- bdd/practibot.sql
+++ bdd/practibot.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Lun 06 Mars 2023 à 18:37
+-- Généré le :  Lun 06 Mars 2023 à 21:30
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -37,22 +37,29 @@ CREATE TABLE `patient` (
   `tel` int(10) DEFAULT NULL,
   `departement` int(3) NOT NULL,
   `medecin_traitant` int(4) NOT NULL,
-  `s1` tinyint(1) NOT NULL,
-  `s2` tinyint(1) NOT NULL,
-  `s3` tinyint(1) NOT NULL,
-  `s4` tinyint(1) NOT NULL,
-  `s5` tinyint(1) NOT NULL,
-  `s6` tinyint(1) NOT NULL,
-  `s7` tinyint(1) NOT NULL,
-  `s8` tinyint(1) NOT NULL,
-  `s9` tinyint(1) NOT NULL,
-  `s10` tinyint(1) NOT NULL,
-  `s11` tinyint(1) NOT NULL,
-  `s12` tinyint(1) NOT NULL,
-  `s13` tinyint(1) NOT NULL,
-  `s14` tinyint(1) NOT NULL,
-  `s15` tinyint(1) NOT NULL
+  `s1` tinyint(1) DEFAULT NULL,
+  `s2` tinyint(1) DEFAULT NULL,
+  `s3` tinyint(1) DEFAULT NULL,
+  `s4` tinyint(1) DEFAULT NULL,
+  `s5` tinyint(1) DEFAULT NULL,
+  `s6` tinyint(1) DEFAULT NULL,
+  `s7` tinyint(1) DEFAULT NULL,
+  `s8` tinyint(1) DEFAULT NULL,
+  `s9` tinyint(1) DEFAULT NULL,
+  `s10` tinyint(1) DEFAULT NULL,
+  `s11` tinyint(1) DEFAULT NULL,
+  `s12` tinyint(1) DEFAULT NULL,
+  `s13` tinyint(1) DEFAULT NULL,
+  `s14` tinyint(1) DEFAULT NULL,
+  `s15` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `patient`
+--
+
+INSERT INTO `patient` (`id_patient`, `nom`, `prenom`, `age`, `mail`, `tel`, `departement`, `medecin_traitant`, `s1`, `s2`, `s3`, `s4`, `s5`, `s6`, `s7`, `s8`, `s9`, `s10`, `s11`, `s12`, `s13`, `s14`, `s15`) VALUES
+(1, 'Lebrun', 'Francis', 45, 'lebrunf@gmail.com', NULL, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -67,9 +74,16 @@ CREATE TABLE `praticien` (
   `email` varchar(50) NOT NULL,
   `specialite` int(1) NOT NULL,
   `departement` int(3) NOT NULL,
-  `login` varchar(50) NOT NULL,
+  `log` varchar(50) NOT NULL,
   `mdp` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `praticien`
+--
+
+INSERT INTO `praticien` (`id_praticien`, `nom`, `prenom`, `email`, `specialite`, `departement`, `log`, `mdp`) VALUES
+(6, 'nom', 'prenom', 'email@gmail.com', 1, 15, 'login', 'mdp');
 
 --
 -- Index pour les tables exportées
@@ -95,12 +109,12 @@ ALTER TABLE `praticien`
 -- AUTO_INCREMENT pour la table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id_patient` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_patient` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `praticien`
 --
 ALTER TABLE `praticien`
-  MODIFY `id_praticien` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_praticien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,66 @@
+<?php
+    include "../components/bdd_connexion.php";
+
+    if (!empty($_POST)) {
+
+        if (!empty($_POST['nom'])){
+
+            //Récupération des données
+
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+            $email = $_POST['email'];
+            $specialite = $_POST['specialite'];
+            $departement = $_POST['departement'];
+            $log = $_POST['log'];
+            $mdp = $_POST['mdp'];
+
+            // Préparation de la requête préparée
+            $sql = "INSERT INTO praticien (nom, prenom, email, specialite, departement, log, mdp) VALUES (:nom, :prenom, :email, :specialite, :departement, :log, :mdp)";
+            $stmt = $conn->prepare($sql);
+
+            // Liaison des variables avec les paramètres de la requête préparée
+            $stmt->bindParam(':nom', $nom);
+            $stmt->bindParam(':prenom', $prenom);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':specialite', $specialite);
+            $stmt->bindParam(':departement', $departement);
+            $stmt->bindParam(':log', $log);
+            $stmt->bindParam(':mdp', $mdp);
+
+            // Exécution de la requête préparée
+            $stmt->execute();
+        }
+        else {
+
+            //Récupération des données
+            
+            $log = $_POST['log'];
+            $mdp = $_POST['mdp'];
+
+            // Préparation de la requête préparée
+
+            $stmt = $conn->prepare("SELECT * FROM praticien WHERE log = :log AND mdp = :mdp");
+            $stmt->execute(['log' => $log, 'mdp' => $mdp]);
+
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if (!$user) {
+                
+                echo "l'utilisateur n'existe pas";
+            }
+            
+            else {
+
+                header('Location: praticien.php');
+                exit;
+            }
+
+        }
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -35,80 +98,6 @@
                 </div>
             </div>
         </form>
-
-        <?php
-            include "../components/bdd_connexion.php";
-
-            if (!empty($_POST)) {
-
-                if (!empty($_POST['nom'])){
-
-                    //Récupération des données
-
-                    $nom = $_POST['nom'];
-                    $prenom = $_POST['prenom'];
-                    $email = $_POST['email'];
-                    $specialite = $_POST['specialite'];
-                    $departement = $_POST['departement'];
-                    $log = $_POST['log'];
-                    $mdp = $_POST['mdp'];
-
-                    var_dump($_POST);
-
-                    //$sql = "INSERT INTO praticien (nom, prenom, email, specialite, departement, log, mdp)
-                    //VALUES ('$nom', '$prenom', '$email', '$specialite', '$departement', '$log', '$mdp');";
-                    //$res = $conn->query($sql);
-
-                    // Préparation de la requête préparée
-                    $sql = "INSERT INTO praticien (nom, prenom, email, specialite, departement, log, mdp) VALUES (:nom, :prenom, :email, :specialite, :departement, :log, :mdp)";
-                    $stmt = $conn->prepare($sql);
-
-                    // Liaison des variables avec les paramètres de la requête préparée
-                    $stmt->bindParam(':nom', $nom);
-                    $stmt->bindParam(':prenom', $prenom);
-                    $stmt->bindParam(':email', $email);
-                    $stmt->bindParam(':specialite', $specialite);
-                    $stmt->bindParam(':departement', $departement);
-                    $stmt->bindParam(':log', $log);
-                    $stmt->bindParam(':mdp', $mdp);
-
-                    // Exécution de la requête préparée
-                    $stmt->execute();
-                }
-                else {
-
-                    //Récupération des données
-                    
-                    $log = $_POST['log'];
-                    $mdp = $_POST['mdp'];
-
-                    var_dump($_POST);
-
-                    // Préparation de la requête préparée
-
-                    $stmt = $conn->prepare("SELECT * FROM praticien WHERE log = :log");
-                    $stmt->execute(['log' => $log]);
-
-                    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-                    if (!$user) {
-                        
-                        echo "l'utilisateur n'existe pas";
-
-                        exit;
-                    }
-
-                    if (!password_verify($mdp, $user['mdp'])) {
-                        
-                        echo "le mot de passe est incorrect";
-
-                        exit;
-                    }
-
-                }
-            }
-        ?>
-
     </main>
 
     <?php
