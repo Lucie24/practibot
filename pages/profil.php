c71af4315680049d3397d4fd9360a7f44b8771d7
Lucie <luciedumas24430@gmail.com>
2023-03-14 06:39:18 +0100

    Amélioration de la page profil.php
    TODO : Faire les requêtes, le bouton de désinscription et le système de fichiers pour l'avatar

--- pages/profil.php
+++ pages/profil.php
@@ -19,10 +19,34 @@
             <h2>Mon profil</h2>
         </div>
 
+        <?php
+        #Faire la récupération des infos dans la base de données pour les afficher en tant que placeholder dans les input
+        ?>
+
         <div class="containerTab">
-
-
-
+            <form action="profil.php" method="post" role="form">
+                <div class="row">
+                    <div class="col">
+                        <p>Votre photo de profil</p>
+                    </div>
+                    <div class="col">
+                        <p>Vos informations personnelles</p>
+                        <div class="form-group">
+                            <input type="text" name="id" class="form-control" id="id" placeholder="Nom">
+                        </div>
+                        <div class="form-group marginThinTop">
+                            <input type="password" name="mdp" class="form-control" id="mdp" placeholder="Prénom">
+                        </div>
+                        <div class="form-group marginThinTop">
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                        </div>
+                    </div>
+                    <div class="text-end marginThinTop">
+                        <button type="submit" class="btn btnPrimary">Modifier</button>
+                    </div>
+                </div>
+                <p class="textLeft">Votre compte a été créé le *insérer timestamp*</p>
+            </form>
         </div>
 
     </main>
