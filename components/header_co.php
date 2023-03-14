5eb82390c30500c089ddd4b6ef0bf5df3296d12d
Lucie <luciedumas24430@gmail.com>
2023-03-14 05:50:08 +0100

    (création du header_co et du profil qui ne s'étaient pas envoyés)

---
+++ components/header_co.php
@@ -0,0 +1,46 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+        <h1 class="logo me-auto"><a href="../index.php">PractiBot</a></h1>
+
+        <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+                <li><a class="nav-link scrollto" href="praticien.php">Tableau de bord</a></li>
+                <li><a class="nav-link scrollto" href="profil.php">Mon profil</a></li>
+                <li>
+                    <button type="button" class="btn btnPrimary" data-bs-toggle="modal" data-bs-target="#popup">
+                        Se déconnecter
+                    </button>
+                </li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- navbar -->
+    </div>
+</header><!-- End Header -->
+
+<!-- ======= Popup formulaires ======= -->
+<div class="modal fade" id="popup" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Espace praticien</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center flexRow">
+                <div class="container text-center">
+                    <div class="row align-items-center">
+                        <p>Souhaitez-vous vous déconnecter ?</p>
+                        <div class="col">
+                            <a href="signup.php" class="btn btnPrimary">Oui</a>
+                        </div>
+                        <div class="col">
+                            <button type="button" class="btn btnSecundary" data-bs-dismiss="modal" aria-label="Close">Non</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
---
+++ pages/profil.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php
+    #Head
+    include '../components/head.html';
+?>
+
+<body class="section-bg">
+
+<?php
+    #Header
+    include '../components/header_co.php';
+?>
+
+    <main class="marginFatTop">
+
+        <div class="section-title">
+            <h2>Mon profil</h2>
+        </div>
+
+        <div class="containerTab">
+
+
+
+        </div>
+
+    </main>
+
+    <?php
+        #Footer
+        include "../components/footer.html";
+        include "../components/buttonTop.html";
+        include "../components/scripts.html";
+    ?>
+
+</body>
+
+</html>
