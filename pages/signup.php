9b7f155100d0334187b9d664a4f0c2a381848601
Lucie <luciedumas24430@gmail.com>
2023-03-19 11:11:54 +0100

    rajout de vérifications pour l'existence de l'id et de l'email

--- pages/signup.php
+++ pages/signup.php
@@ -2,58 +2,98 @@
 
     include "../components/bdd_connexion.php";
     
+    // Si la variable POST contient des informations
     if (!empty($_POST)){
+
+        // Si la variable POST contient la valeur d'id
         if (!empty($_POST['id'])){
+
+            // Si la variable POST contient la valeur de mdp
             if (!empty($_POST['mdp'])){
+                
+                // Si la variable POST contient la valeur de mdp2
                 if (!empty($_POST['mdp2'])){
+                    
+                    // Si la variable POST contient la valeur de nom
                     if (!empty($_POST['nom'])){
+                        
+                        // Si la variable POST contient la valeur de prénom
                         if (!empty($_POST['prenom'])){
+
+                            // Si la variable POST contient la valeur d'email
                             if (!empty($_POST['email'])){
+                                
+                                // Si la variable POST contient la valeur de département
                                 if (!empty($_POST['departement'])){
+                                    
+                                    // Si la variable POST contient la valeur de spécialité
                                     if (!empty($_POST['specialite'])){
 
-                                            // Récupération des données
+                                        // Récupération des données
+                                        $nom = htmlspecialchars($_POST['nom']);
+                                        $prenom = htmlspecialchars($_POST['prenom']);
+                                        $email = htmlspecialchars($_POST['email']);
+                                        $specialite = htmlspecialchars($_POST['specialite']);
+                                        $departement = htmlspecialchars($_POST['departement']);
+                                        $id = htmlspecialchars($_POST['id']);
+                                        $mdp = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
 
-                                            $nom = htmlspecialchars($_POST['nom']);
-                                            $prenom = htmlspecialchars($_POST['prenom']);
-                                            $email = htmlspecialchars($_POST['email']);
-                                            $specialite = htmlspecialchars($_POST['specialite']);
-                                            $departement = htmlspecialchars($_POST['departement']);
-                                            $id = htmlspecialchars($_POST['id']);
-                                            $mdp = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
+                                        // Préparation de la requête préparée permettant de tester si le nom d'utilisateur est déjà utilisé
+                                        $stmt = $conn->prepare("SELECT id FROM praticien WHERE id = :id");
+                                        $stmt->execute(['id' => $id]);
 
-                                        if ($_POST['mdp'] == $_POST['mdp2']){
+                                        $idInBdd = $stmt->fetch(PDO::FETCH_ASSOC);
 
-                                            // Préparation de la requête préparée
-                                            $sql = "INSERT INTO praticien (nom, prenom, email, specialite, departement, id, mdp) VALUES (:nom, :prenom, :email, :specialite, :departement, :id, :mdp)";
-                                            $stmt = $conn->prepare($sql);
+                                        // Si l'id n'est pas dans la base de données
+                                        if (!$idInBdd) {
 
-                                            // Liaison des variables avec les paramètres de la requête préparée
-                                            $stmt->bindParam(':nom', $nom);
-                                            $stmt->bindParam(':prenom', $prenom);
-                                            $stmt->bindParam(':email', $email);
-                                            $stmt->bindParam(':specialite', $specialite);
-                                            $stmt->bindParam(':departement', $departement);
-                                            $stmt->bindParam(':id', $id);
-                                            $stmt->bindParam(':mdp', $mdp);
+                                            // Préparation de la requête préparée permettant de tester si l'adresse mail est déjà utiliséé
+                                            $stmt = $conn->prepare("SELECT email FROM praticien WHERE email = :email");
+                                            $stmt->execute(['email' => $email]);
 
-                                            // Exécution de la requête préparée
-                                            $stmt->execute();
+                                            $emailInBdd = $stmt->fetch(PDO::FETCH_ASSOC);
 
-                                            if (!$stmt) {
-                                                echo "\nPDO::errorInfo():\n";
-                                                print_r($dbh->errorInfo());
+                                            // Si l'email n'est pas dans la base de données
+                                            if (!$emailInBdd) {
+
+                                                // Si les mots de passes correspondent
+                                                if ($_POST['mdp'] == $_POST['mdp2']){
+
+                                                    // Préparation de la requête préparée
+                                                    $sql = "INSERT INTO praticien (nom, prenom, email, specialite, departement, id, mdp) VALUES (:nom, :prenom, :email, :specialite, :departement, :id, :mdp)";
+                                                    $stmt = $conn->prepare($sql);
+        
+                                                    // Liaison des variables avec les paramètres de la requête préparée
+                                                    $stmt->bindParam(':nom', $nom);
+                                                    $stmt->bindParam(':prenom', $prenom);
+                                                    $stmt->bindParam(':email', $email);
+                                                    $stmt->bindParam(':specialite', $specialite);
+                                                    $stmt->bindParam(':departement', $departement);
+                                                    $stmt->bindParam(':id', $id);
+                                                    $stmt->bindParam(':mdp', $mdp);
+        
+                                                    // Exécution de la requête préparée
+                                                    $stmt->execute();
+                                                    
+                                                    // Vide la variable et redirige l'utilisateur sur la page login.php
+                                                    unset($_POST);
+                                                    header('Location: login.php');
+        
+                                                }
+        
+                                                else {
+                                                    $notcorrespmdp = true;
+                                                }              
                                             }
-                                            
-                                            // Vide la variable et redirige l'utilisateur sur la page login.php
-                                            unset($_POST);
-                                            header('Location: login.php');
 
+                                            else {
+                                                $emailexists = true;
+                                            }
                                         }
 
                                         else {
-                                            $notcorrespmdp = true;
-                                        }              
+                                            $idexists = true;
+                                        }
                                     }
 
                                     else {
@@ -128,6 +168,9 @@
                     if (isset($notid)) {
                         echo "<p class='red'>Vous n'avez pas entré votre identifiant</p>";
                     }
+                    if (isset($idexists)) {
+                        echo "<p class='red'>Cet identifiant n'est pas disponible</p>";
+                    }
                 ?>
 
                 <div class="form-group marginThinTop">
@@ -184,6 +227,10 @@
                 <?php
                     if (isset($notemail)) {
                         echo "<p class='red'>Vous n'avez pas entré votre adresse mail</p>";
+                    }
+
+                    if (isset($emailexists)) {
+                        echo "<p class='red'>Cette adresse mail est déjà utilisée</p>";
                     }
                 ?>
 
