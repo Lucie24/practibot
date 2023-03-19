0a72d876c4c17c4ec666d5311bb8fa0b05ccd01c
Lucie <luciedumas24430@gmail.com>
2023-03-19 15:32:28 +0100

    maj session

--- components/verif_conn.php
+++ components/verif_conn.php
@@ -1,21 +1,10 @@
 <?php
 
-function sessionstart () {
-    // Nous vérifions que l'utilisateur soit bien connecté pour accéder à cette page.
-    // S'il ne l'est pas, nous le redirigeons vers la page login.php
-    if (!isset($_SESSION['conn']) || $_SESSION['conn'] !== true) {
-    
-        header('Location: login.php');
-        exit();
-    }
-}
+// Nous vérifions que l'utilisateur soit bien connecté pour accéder à cette page.
+// S'il ne l'est pas, nous le redirigeons vers la page login.php
+if (!isset($_SESSION['conn']) || $_SESSION['conn'] !== true) {
 
-function sessiondestroy () {
-    session_start();
-
-    if ($_SESSION['conn'] == true) {
-    
-        session_destroy();
-    }
+    header('Location: login.php');
+    exit();
 }
 ?>
